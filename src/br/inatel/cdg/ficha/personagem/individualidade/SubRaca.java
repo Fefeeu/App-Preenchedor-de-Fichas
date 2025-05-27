24e3a2f903ad832b5d35d98940f3d1ebaf37a5f9
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-05-27 13:44:40 -0300

    iniciando o back

--- src/br/inatel/cdg/ficha/personagem/individualidade/SubRaca.java
+++
@@ -1,8 +0,0 @@
-package br.inatel.cdg.ficha.personagem.individualidade;
-
-public class SubRaca extends Individualidade {
-
-    public SubRaca(String nome, String proficiencia, String[] beneficios) {
-        super(nome, proficiencia, beneficios);
-    }
-}
