777d414392514907dba8b8348132018c3e455363
Felipe Ferreira <felipefcgp.se@gmail.com>
2025-06-24 17:58:41 -0300

    organizando o BD

--- back-end/banco-de-dados/rpp_BD.sql
+++ back-end/banco-de-dados/rpp_BD.sql
@@ -9,32 +9,7 @@ CREATE TABLE `classe` (
   `idClasse` varchar(45) NOT NULL,
   `nome` varchar(45) DEFAULT NULL,
   `descricao` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Despejando dados para a tabela `classe`
---
-
-INSERT INTO `classe` (`idClasse`, `nome`, `descricao`) VALUES
-('artifice', 'Artífice', 'Inventor de engenhocas mágicas'),
-('barbaro', 'Bárbaro', 'Guerreiro feroz com fúria em batalha'),
-('bardo', 'Bardo', 'Artista mágico que inspira aliados'),
-('bruxo', 'Bruxo', 'Conjurador com pacto sobrenatural'),
-('clerigo', 'Clérigo', 'Devoto com magias divinas'),
-('druida', 'Druida', 'Guardião da natureza que se transforma'),
-('feiticeiro', 'Feiticeiro', 'Magia inata no sangue'),
-('guerreiro', 'Guerreiro', 'Mestre em combate físico'),
-('ladino', 'Ladino', 'Especialista em furtividade'),
-('mago', 'Mago', 'Estudioso de magias arcanas'),
-('monge', 'Monge', 'Guerreiro espiritual com artes marciais'),
-('paladino', 'Paladino', 'Cavaleiro sagrado com juramentos'),
-('patrulheiro', 'Patrulheiro', 'Caçador e explorador');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `ficha`
---
+);
 
 CREATE TABLE `ficha` (
   `idFicha` int(11) NOT NULL,
@@ -98,13 +73,7 @@ CREATE TABLE `ficha` (
   `proficiencias` varchar(100) NOT NULL DEFAULT '',
   `inventario_idInventario` int(11) DEFAULT NULL,
   `magiaUser_idMagiaUser` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `inventario`
---
+);
 
 CREATE TABLE `inventario` (
   `idInventario` int(11) NOT NULL,
@@ -113,13 +82,7 @@ CREATE TABLE `inventario` (
   `pe` int(11) NOT NULL DEFAULT 0,
   `po` int(11) NOT NULL DEFAULT 0,
   `pl` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `item`
---
+);
 
 CREATE TABLE `item` (
   `idItem` int(11) NOT NULL,
@@ -139,13 +102,7 @@ CREATE TABLE `item` (
   `numeroDeDados` int(11) DEFAULT NULL,
   `dadoDeDano` int(11) DEFAULT NULL,
   `atributo` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `magia`
---
+);
 
 CREATE TABLE `magia` (
   `idMagia` int(11) NOT NULL,
@@ -162,13 +119,7 @@ CREATE TABLE `magia` (
   `tipoAcerto` enum('resistencia','teste','nenhum') NOT NULL DEFAULT 'nenhum',
   `ladoDado` int(11) NOT NULL DEFAULT 6,
   `numeroDados` int(11) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `magiauser`
---
+);
 
 CREATE TABLE `magiauser` (
   `idMagiaUser` int(11) NOT NULL,
@@ -181,68 +132,24 @@ CREATE TABLE `magiauser` (
   `nivel7` int(11) NOT NULL DEFAULT 0,
   `nivel8` int(11) NOT NULL DEFAULT 0,
   `nivel9` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `raca`
---
+);
 
 CREATE TABLE `raca` (
   `idRaca` varchar(45) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `descricao` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Despejando dados para a tabela `raca`
---
-
-INSERT INTO `raca` (`idRaca`, `nome`, `descricao`) VALUES
-('anao', 'Anão', 'Resistente e habilidoso em montanhas'),
-('draconato', 'Draconato', 'Híbrido humanoide com traços de dragão'),
-('elfo', 'Elfo', 'Gracioso e com afinidade arcana'),
-('gnomo', 'Gnomo', 'Pequeno, inventivo e ilusionista'),
-('halfling', 'Halfling', 'Pequeno, ágil e sortudo'),
-('humano', 'Humano', 'Adaptável e versátil'),
-('meio-elfo', 'Meio-Elfo', 'Mistura de humano e elfo, diplomata'),
-('meio-orc', 'Meio-Orc', 'Forte e resistente, com herança orc'),
-('tiefling', 'Tiefling', 'Herança infernal e poderes sombrios');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `user`
---
+);
 
 CREATE TABLE `user` (
   `idUser` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+);
 
---
--- Despejando dados para a tabela `user`
---
-
-INSERT INTO `user` (`idUser`, `nome`, `email`, `senha`) VALUES
-(1, 'delamain', 'delamain@nc.com', 'delamain123');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `classe`
---
 ALTER TABLE `classe`
   ADD PRIMARY KEY (`idClasse`);
 
---
--- Índices de tabela `ficha`
---
 ALTER TABLE `ficha`
   ADD PRIMARY KEY (`idFicha`,`user_idUser`),
   ADD KEY `raca_idRaca` (`raca_idRaca`),
@@ -251,51 +158,26 @@ ALTER TABLE `ficha`
   ADD KEY `user_idUser` (`user_idUser`),
   ADD KEY `magiaUser_idMagiaUser` (`magiaUser_idMagiaUser`);
 
---
--- Índices de tabela `inventario`
---
 ALTER TABLE `inventario`
   ADD PRIMARY KEY (`idInventario`);
 
---
--- Índices de tabela `item`
---
 ALTER TABLE `item`
   ADD PRIMARY KEY (`idItem`,`inventario_idInventario`),
   ADD KEY `inventario_idInventario` (`inventario_idInventario`);
 
---
--- Índices de tabela `magia`
---
 ALTER TABLE `magia`
   ADD PRIMARY KEY (`idMagia`,`magiaUser_idMagiaUser`),
   ADD KEY `magiaUser_idMagiaUser` (`magiaUser_idMagiaUser`);
 
---
--- Índices de tabela `magiauser`
---
 ALTER TABLE `magiauser`
   ADD PRIMARY KEY (`idMagiaUser`);
 
---
--- Índices de tabela `raca`
---
 ALTER TABLE `raca`
   ADD PRIMARY KEY (`idRaca`);
 
---
--- Índices de tabela `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`idUser`);
 
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `ficha`
---
 ALTER TABLE `ficha`
   ADD CONSTRAINT `ficha_ibfk_1` FOREIGN KEY (`raca_idRaca`) REFERENCES `raca` (`idRaca`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `ficha_ibfk_2` FOREIGN KEY (`classe_idClasse`) REFERENCES `classe` (`idClasse`) ON DELETE SET NULL ON UPDATE CASCADE,
@@ -303,15 +185,8 @@ ALTER TABLE `ficha`
   ADD CONSTRAINT `ficha_ibfk_4` FOREIGN KEY (`user_idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `ficha_ibfk_5` FOREIGN KEY (`magiaUser_idMagiaUser`) REFERENCES `magiauser` (`idMagiaUser`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
---
--- Restrições para tabelas `item`
---
 ALTER TABLE `item`
   ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`inventario_idInventario`) REFERENCES `inventario` (`idInventario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Restrições para tabelas `magia`
---
 ALTER TABLE `magia`
   ADD CONSTRAINT `magia_ibfk_1` FOREIGN KEY (`magiaUser_idMagiaUser`) REFERENCES `magiauser` (`idMagiaUser`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
